e9be208f41fa21cea31dd3cd12dc05c23b3b804e
Ujjal Roy <royu99099@gmail.com>
2025-01-24 19:23:33 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using node js ,not browser
+
+let name="Ujjal"
+console.log(typeof null);
+
+console.log(typeof undefined);
