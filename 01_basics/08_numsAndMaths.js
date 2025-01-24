d3466e1c9c7debad5acebb2eb514183a80f137aa
Ujjal Roy <royu99099@gmail.com>
2025-01-24 21:46:21 +0000

    Maths and Num

---
+++ 01_basics/08_numsAndMaths.js
@@ -0,0 +1,27 @@
+const score=400;
+//console.log(score);
+
+const balance=new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//to set the decimal places like 400.00 
+
+const hundreds=100000;
+//console.log(hundreds.toLocaleString());//to represent the hundred with comma separated
+
+//----------------Maths-------------------
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.round(5.4));
+
+// console.log(Math.random());//it gives values from 0 to 1
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);//gives value from 1 to 9(+1 to avoid the value getting 0)
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
