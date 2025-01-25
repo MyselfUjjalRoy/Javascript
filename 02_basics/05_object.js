a3035b84bd2b0d141649f720cb910e64d476cb0c
Ujjal Roy <royu99099@gmail.com>
2025-01-25 10:58:10 +0000

    Destructuring in js

---
+++ 02_basics/05_object.js
@@ -0,0 +1,15 @@
+const course={
+    coursename:"Js in hindi",
+    price:"999",
+    courseInstructor:"Ujjal"
+}
+
+// const{courseInstructor}=course
+// console.log(courseInstructor);
+
+const{courseInstructor:instructor}=course//this way i can rename  a long variable name to a short name
+console.log(instructor);
+//this is called destructuring
+
+
+
