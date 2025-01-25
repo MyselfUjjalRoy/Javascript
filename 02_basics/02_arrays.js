ccb02427262d134c5a0021f9a7b1d28f33f014dd
Ujjal Roy <royu99099@gmail.com>
2025-01-25 10:16:57 +0000

    objects member acces and add symbol

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const odd=[1,3,5]
+const even=[2,4,6]
+// odd.push(even);//push method add elements in the existing array
+// console.log(odd);
+// console.log(odd[3][1]);
+
+// const all=odd.concat(even)//concat method returns a new array
+
+
+// const all=[...odd,...even]//using spread operator
+ // console.log(all);
+
+// const newArr=[1,2,[4,5,6],7,[8,8,[10,11]]]
+// const main_newArr=newArr.flat(Infinity)
+// console.log(main_newArr);//output:-[
+//     1, 2, 4,  5,  6,
+//     7, 8, 8, 10, 11
+//   ]
+let score1=10
+let score2=20
+let score3=30
+console.log(Array.of(score1,score2,score3));
+
+
+
