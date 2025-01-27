0ba0bee19b5de05d94b54d16a9dd1f313afa3719
Ujjal Roy <royu99099@gmail.com>
2025-01-27 18:24:32 +0000

    for loop ,break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,29 @@
+//for
+
+// for(let i=0;i<10;i++){
+//     const element=i;
+//     console.log(element);
+    
+// }
+
+// let array=["one","two","three","four","five"]
+// console.log(array.length);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+
+//break and continue
+
+for (let index = 0; index <20; index++) {
+    if(index==5){
+        console.log(`Detected 5`);
+        // break;
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+    
+    
+}
