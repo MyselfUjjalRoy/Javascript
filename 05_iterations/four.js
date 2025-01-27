62b8e7504151f668bf870408900c2cda984c2572
Ujjal Roy <royu99099@gmail.com>
2025-01-27 19:11:11 +0000

    forin loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+const myObj={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+
+}
+
+//for in loop
+
+// for (const key in myObj) {
+//     //console.log(key);
+//     console.log(`${key} shortcut is for ${myObj[key]}`);//this way we cann acces the key value pair
+    
+// }
+
+// const programming=["js","cpp","python"]
+// for (const key in programming) {
+//   // console.log(key);//but yaha index print hoga,uska values nahi
+//    console.log(programming[key]);//to print the elements
+   
+// }
+
+// const map=new Map()
+// map.set('IN',"India")
+// map.set('US',"United States")
+// map.set('NEP',"Nepal")
+
+// for (const key in map) {
+//    console.log(key);//in this way map is not iterable,so nothing will be printed
+   
+// }
+
