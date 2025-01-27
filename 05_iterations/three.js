31de59dbc7349c19b21fc083ae8a4ea510c11856
Ujjal Roy <royu99099@gmail.com>
2025-01-27 18:59:21 +0000

    forof loop and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,38 @@
+//for of
+
+// const arr=[1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);   
+// }
+
+// const greetings="hello World"
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+    
+// }
+
+
+//Map--Stores only unique values
+
+const map=new Map()
+map.set('IN',"India")
+map.set('US',"United States")
+map.set('IN',"India")//duplicate value store nahi karte hai
+map.set('NEP',"Nepal")
+//console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,':-',value);
+    
+// }
+
+// const myObj={
+//     'game1':'NFS',
+//     'game2':'PUBG'
+// }
+// for (const [key,value] of myObj) {//forof loop se object ko iterate nahi kar sakte hai//uska alag tarika hai
+//     console.log(key,':-',value);
+    
+// }
+
